5f3941735182dd64ab4fe0b78a1016005fff8fe5
izitouni-54 <izitouni336@gmail.com>
2024-12-07 10:36:38 +0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,14 @@
+
+let products = [
+    { subject: 'Painting and Drawing', location: 'Oxford', price: 100, spaces: 10, image: 'images/painting.jpg', rating: 3.75 },
+    { subject: 'Yoga and Pilates', location: 'Oxford', price: 100, spaces: 10, image: 'images/yoga and pilates.jpg', rating: 3.75 },
+    { subject: 'Sculpting', location: 'York', price: 80, spaces: 10, image: 'images/sculpting 1.jpg', rating: 4.75 },
+    { subject: 'Crafting', location: 'Bristol', price: 90, spaces: 10, image: 'images/crafting.jpg', rating: 4.75 },
+    { subject: 'Photography', location: 'Manchester', price: 40, spaces: 10, image: 'images/photography.jpg', rating: 2.75 },
+    { subject: 'Fashion Design', location: 'Macclesfield', price: 50, spaces: 10, image: 'images/fashion design.jpg', rating: 2.75 },
+    { subject: 'Football', location: 'Manchester', price: 40, spaces: 10, image: 'images/football.jpg', rating: 3.50 },
+    { subject: 'Basketball', location: 'London', price: 40, spaces: 10, image: 'images/basketball.jpg', rating: 3.50 },
+    { subject: 'Tennis', location: 'London', price: 30, spaces: 10, image: 'images/tennis.jpg', rating: 3.0 },
+    { subject: 'Badminton', location: 'Middlesbrough', price: 30, spaces: 10, image: 'images/badminton.jpg', rating: 2.50 },
+    { subject: 'Rugby', location: 'Macclesfield', price: 60, spaces: 10, image: 'images/rugby.jpg', rating: 3.50 }
+];
